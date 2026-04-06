1af59df9d58b76eb236cbfdda5a83f8ee4939172
mars <imidcen@gmail.com>
2026-04-06 18:51:29 +0100

    separated toolbar

--- src/layout/ToolBar.jsx
+++ src/layout/ToolBar.jsx
@@ -1,10 +1,51 @@
-function ToolBar({ darkMode }) {
-    
+function Toolbar({ 
+    activeCursor,
+    setActiveCursor,
+    toggleEditPanel,
+    addToCanvas,
+    darkMode,
+    setDarkMode
+}) {
     return (
-        <>
-            
-        </>
-    );
-}
+        <div className="relative z-10">
+            <div className="w-1/3 absolute left-1/2 -translate-x-1/2 bottom-8">
+                <div className={`py-2 px-8 flex items-center justify-between rounded-2xl fa-xl
+                    ${darkMode ? "text-[#EBFFF2] bg-[#1F1F1F]" : "text-[#1F1F1F] bg-[#EBFFF2]"}`}
+                >
+                    {/* for editing on canvas */}
+                    <button
+                        className={`fa fa-arrow-pointer cursor-pointer hover:text-[#B5446E] ${activeCursor=== 'pointer' ? 'text-[#B5446E]' : ''}`}
+                        onClick={() => setActiveCursor('pointer')}
+                    />
 
-export default ToolBar;
+                    {/* for grabbing and moving around canvas */}
+                    <button
+                        className={`fa fa-hand cursor-pointer hover:text-[#B5446E] ${activeCursor=== 'pointer' ? '' : 'text-[#B5446E]'}`}
+                        onClick={() => setActiveCursor('hand')}
+                    />
+
+                    {/* for new page */}
+                    <i className="fa fa-file-circle-plus hover:text-[#B5446E]" onClick={() => toggleEditPanel('pages')}></i>
+
+                    {/* for new shape */}
+                    <i className="fa fa-draw-polygon cursor-pointer hover:text-[#B5446E]" onClick={() => toggleEditPanel('shapes')}></i>
+
+                    {/* for adding text */}
+                    <button className="fa fa-font cursor-pointer hover:text-[#B5446E]" onClick={() => addToCanvas('text')} />
+
+                    <div className={`inline-block h-full min-h-[2em] w-1 self-stretch rounded-full ${darkMode ? "bg-[#EBFFF2]" : "bg-[#1F1F1F]"}`}></div>
+
+                    <div className={`flex flex-row items-center px-2 py-1 border-3 rounded-md text-lg font-fustat-semibold space-x-2 ${darkMode ? "border-[#EBFFF2]" : "border-[#1F1F1F]"}`}>
+                        <i className="fa-solid fa-magnifying-glass fa-sm"></i>
+                        <p>60%</p>
+                    </div>
+
+                    {/* toggling dark and light mode */}
+                    <button onClick={() => setDarkMode(!darkMode)} className={`hover:text-[#B5446E] cursor-pointer fa ${darkMode ? "fa-sun" : "fa-moon"}`} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Toolbar;
