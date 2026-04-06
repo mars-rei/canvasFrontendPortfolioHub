4b74cc6f2cc28e96a39b466b3c2d1be38e81e6fd
mars <imidcen@gmail.com>
2026-04-06 21:32:58 +0100

    moved right side bar to it's own component file and added undo and redo icons

--- src/layout/ToolBar.jsx
+++ src/layout/ToolBar.jsx
@@ -12,6 +12,18 @@ function Toolbar({
                 <div className={`py-2 px-8 flex items-center justify-between rounded-2xl fa-xl
                     ${darkMode ? "text-[#EBFFF2] bg-[#1F1F1F]" : "text-[#1F1F1F] bg-[#EBFFF2]"}`}
                 >
+                    {/* undo */}
+                    <button
+                        className={`fa fa-rotate-left cursor-pointer hover:text-[#B5446E]`}
+                    />
+
+                    {/* redo */}
+                    <button
+                        className={`fa fa-rotate-right cursor-pointer hover:text-[#B5446E]`}
+                    />
+
+                    <div className={`inline-block h-full min-h-[2em] w-1 self-stretch rounded-full ${darkMode ? "bg-[#EBFFF2]" : "bg-[#1F1F1F]"}`}></div>
+
                     {/* for editing on canvas */}
                     <button
                         className={`fa fa-arrow-pointer cursor-pointer hover:text-[#B5446E] ${activeCursor=== 'pointer' ? 'text-[#B5446E]' : ''}`}
